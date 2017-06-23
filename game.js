5c4837be3f1ac23a01fccffd64def249bd1e993f
Igor Lobanov <bniwredyc@gmail.com>
2017-06-23 13:32:55 +0300

    Review 1

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ class Vector {
   }
   
   plus(vectorObj) {
+    // instanceof и обратить условие
     if (Vector.prototype.isPrototypeOf(vectorObj)) {
       let x = this.x + vectorObj.x,
           y = this.y + vectorObj.y;
@@ -21,14 +22,18 @@ class Vector {
 
 class Actor {
   constructor(pos = new Vector(), size = new Vector(1, 1), speed = new Vector()) {
+    // instanceof
     if (!this.isVectorProto(pos) || !this.isVectorProto(size) || !this.isVectorProto(speed)) {
       throw(new Error('Переданный аргумент неверного типа!'));
+      // else не нужен, после исключение выполнения функции прекратится
     } else {
       this.pos   = pos;
       this.size  = size;
       this.speed = speed;
       
+      // лучше перенести в type
       this._type   = 'actor';
+      // лишние поля класса и они не будут меняться при изменении позиции
       this._left   = pos.x;
       this._right  = pos.x + size.x;
       this._top    = pos.y;
@@ -58,15 +63,18 @@ class Actor {
   
   act() {}
   
+  // лишний метод
   isVectorProto(object) {
     return Vector.prototype.isPrototypeOf(object);
   }
   
   isIntersect(actor) {
+    // instanceof, обратить условие
     if (actor && Actor.prototype.isPrototypeOf(actor)) {
       if (actor === this) {
         return false;
       }
+      // если обхект выше, ниже, левее, правее - false, иначе true (упростить)
       return !this.check(actor, this);
       
     } else {
@@ -74,6 +82,7 @@ class Actor {
     }
   }
   
+  // лишний метод
   check(firstActor, secondActor) {
     let first  = this.setSystemCoordinate(firstActor);
     let second = this.setSystemCoordinate(secondActor);
@@ -84,6 +93,7 @@ class Actor {
     }
   }
   
+  // совсем лишний метод
   setSystemCoordinate(actor) {
     let left, bottom;
     let newActor = {
@@ -108,12 +118,17 @@ class Actor {
 
 class Level {
   constructor(grid = [], actors = []) {
+    // не нужно так форматировать - любой автоматический форматтер сбросит это
+    // тут нужно создать копию массива, чтобы нельзя было изменить поле снаружи
     this.grid        = grid;
     this.height      = grid.length;
+    // 0 достаточно передать первым аргументов в max
     this.width       = grid.length > 0 ? Math.max(...grid.map(item => item.length)) : 0;
     this.status      = null;
     this.finishDelay = 1;
+    // осздать копию
     this.actors      = actors;
+    // игрок может быть не первым в массиве
     this.player      = actors.length > 0 ? actors[0] : null;
   }
   
@@ -125,6 +140,7 @@ class Level {
     if (!Actor.prototype.isPrototypeOf(actor)) {
       throw(new Error('Передан не верный аргумент!'));
     }
+    // тут нужно провто найти в массиве элемент, который пересекается с переданным
     if (this.actors.length && !(this.actors.length === 1) && actor.isIntersect(this.actors[1])) {
       return this.actors[1];
     }
@@ -134,15 +150,20 @@ class Level {
     if (!Vector.prototype.isPrototypeOf(position) || !Vector.prototype.isPrototypeOf(size)) {
       throw(new Error('Передан не верный аргумент!'));
     }
+
+    // тут должно быть 2 цикла по вертикали и горизонтали,
+    // передираем ячейки поля и возвращаем значение, если ячейка не пуста
     let object = this.getVectorObject(position, size);
     if (object.left < 0 || object.right >= this.width || object.top >= this.height
       || !this.isCeil(object) || this.grid[object.left][object.bottom] === 'wall') {
+        // эти значения должно браться из словаря
       return 'wall';
     } else if (object.bottom < 0 || this.grid[object.left][object.bottom] === 'lava') {
       return 'lava';
     }
   }
   
+  // лишний метод
   getVectorObject(position, size) {
     let object = {};
     if (position.x + size.x > position.x) {
@@ -162,6 +183,7 @@ class Level {
     return object;
   }
   
+  // лишний метод
   isCeil(object) {
     for (let value of Object.keys(object)) {
       if (Math.ceil(object[value]) !== object[value]) {
@@ -171,6 +193,7 @@ class Level {
     return true
   }
   
+  // это нужно упростить
   removeActor(actor) {
     this.actors = this.actors.reduce((mass, item) => {
       if (item !== actor) {
@@ -181,6 +204,7 @@ class Level {
   }
   
   noMoreActors(type) {
+    // здесь лучше использовать some
     for (let index = 0; index < this.actors.length; index++) {
       if (this.actors[index].type === type) {
         return false;
@@ -189,8 +213,11 @@ class Level {
     return true;
   }
   
+  // перечитайте описание метода, в нём не хватает условия
+  // actor = undefined - некорректное значение по-умолчанию, аргумент и так будет undefined
   playerTouched(touch, actor = undefined) {
     if (touch === 'lava' || touch === 'fireball') {
+      // после этой строчки лучше написать return и убрать else if
       this.status = 'lost';
     } else if (touch === 'coin') {
       this.removeActor(actor);
@@ -202,16 +229,21 @@ class Level {
 }
 
 class LevelParser {
+  // некорректное значение по умолчанию
   constructor(actorObject = undefined) {
     this.actorObject = actorObject;
+    // в словаре должны бить только препятствия
     this.symbols     = ['x', '!', '@', 'o', '=', '|', 'v'];
   }
   
+  // некорректное значение по умолчанию
   actorFromSymbol(symbol = undefined) {
+    // условие лишнее
     return symbol ? this.actorObject[symbol] : undefined;
   }
   
   obstacleFromSymbol(symbol = undefined) {
+    // здесь должен быть объект, а не массив
     if (this.symbols.indexOf(symbol) === -1) {
       return undefined;
     } else if (symbol === 'x') {
@@ -221,7 +253,9 @@ class LevelParser {
     }
   }
   
+  // лучше добавить значение по-умолчанию
   createGrid(plan) {
+    // лишняя проверка
     if (!plan.length) {
       return [];
     }
@@ -229,6 +263,7 @@ class LevelParser {
     return plan;
   }
   
+  // это нужно переписать с учётом, что symbols должен быть массивом
   createActors(actors) {
     if (!actors.length || this.actorObject === undefined) {
       return [];
@@ -239,6 +274,7 @@ class LevelParser {
     return actors;
   }
   
+  // это лишний метод (перенести в createActors)
   filterSymbolsArray(arraySymbols, indexArray) {
     return arraySymbols
       .map((symbol, index) => {
@@ -259,24 +295,31 @@ class LevelParser {
 }
 
 class Fireball extends Actor {
+  // лучше не опускать параметры new Vector - напишите корректные размеры
   constructor(pos = new Vector(), speed = new Vector()) {
+    // вместо undefined подставить корректное значение
     super(pos, undefined, speed);
+    // лишнее поле
     this._type = 'fireball';
   }
   
   getNextPosition(time = 1) {
+    // pos должно быть вектром
     let pos = {};
+    // форматировение
     pos.x   = this.pos.x + (this.speed.x * time);
     pos.y   = this.pos.y + (this.speed.y * time);
     return pos;
   }
   
   handleObstacle() {
+    // мутируете вектор - не надо
     this.speed.x *= -1;
     this.speed.y *= -1;
   }
   
   act(time, level) {
+    // это неправильно (см. выше)
     let pos = new Vector(this.getNextPosition(time).x, this.getNextPosition(time).y);
     if (!level.obstacleAt(pos, this.size)) {
       this.pos = this.getNextPosition(time)
@@ -286,11 +329,13 @@ class Fireball extends Actor {
   }
 }
 class HorizontalFireball extends Fireball {
+  // не опускайте параметры по-умолчанию
   constructor(pos = new Vector()) {
     super(pos, new Vector(2, 0));
   }
 }
 class VerticalFireball extends Fireball {
+  // не опускайте параметры по-умолчанию
   constructor(pos = new Vector()) {
     super(pos, new Vector(0, 2));
   }
@@ -316,6 +361,7 @@ class Coin extends Actor {
   }
   
   updateSpring(time = 1) {
+    // скобки не нужны
     this.spring += (this.springSpeed * time);
   }
   
@@ -336,6 +382,9 @@ class Coin extends Actor {
 class Player extends Actor {
   constructor(pos = new Vector()) {
     super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
+    // перегружайте метод type() вместо использования _type иначе неочевидно что происходит
     this._type = 'player';
   }
 }
+
+// здесь должен быть запуск игры (см. описание задания)
